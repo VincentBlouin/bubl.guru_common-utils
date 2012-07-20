5a952804e278deb9211ec800b1225236fb6a5fec
vincent.blouin@gmail.com <vincent.blouin@gmail.com@40667f71-9be7-da36-2ea2-2722c72b7549>
2012-07-20 23:49:15 +0000

    the suggestion button now stays if there was suggestions previously.

---
+++ src/main/java/org/triple_brain/module/common_utils/JsonUtils.java
@@ -0,0 +1,25 @@
+package org.triple_brain.module.common_utils;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import java.util.Iterator;
+
+/*
+* Copyright Mozilla Public License 1.1
+*/
+public class JsonUtils {
+
+    public static JSONObject merge(JSONObject object1, JSONObject object2) throws JSONException {
+        JSONObject merged = new JSONObject();
+        JSONObject[] objs = new JSONObject[] { object1, object2};
+        for (JSONObject obj : objs) {
+            Iterator it = obj.keys();
+            while (it.hasNext()) {
+                String key = (String)it.next();
+                merged.put(key, obj.get(key));
+            }
+        }
+        return merged;
+    }
+}
