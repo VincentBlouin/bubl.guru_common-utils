fd2f6da3daab56edbedab75f6c2a904af06eb6d5
vincent.blouin@gmail.com <vincent.blouin@gmail.com@40667f71-9be7-da36-2ea2-2722c72b7549>
2012-09-10 23:44:02 +0000

    start using version variable for common dependencies between modules

--- src/main/java/org/triple_brain/module/common_utils/Uris.java
+++ src/main/java/org/triple_brain/module/common_utils/Uris.java
@@ -1,15 +1,24 @@
 package org.triple_brain.module.common_utils;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /*
 * Copyright Mozilla Public License 1.1
 */
-public class CommonUtils {
+public class Uris {
     public static String encodeURL(String URL) throws UnsupportedEncodingException {
         return java.net.URLEncoder.encode(URL, "ISO-8859-1");
     }
     public static String decodeURL(String URL) throws UnsupportedEncodingException{
         return java.net.URLDecoder.decode(URL, "ISO-8859-1");
+    }
+    public static URI get(String uri){
+        try{
+            return new URI(uri);
+        }catch (URISyntaxException e){
+            throw new RuntimeException(e);
+        }
     }
 }
