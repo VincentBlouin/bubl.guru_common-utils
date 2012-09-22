05ad2b006b66d5866ffd6d64c62c2abf7a830123
vincent.blouin@gmail.com <vincent.blouin@gmail.com@40667f71-9be7-da36-2ea2-2722c72b7549>
2012-09-22 03:03:26 +0000

    progress in implement neo4j

--- src/main/java/org/triple_brain/module/common_utils/Uris.java
+++ src/main/java/org/triple_brain/module/common_utils/Uris.java
@@ -3,16 +3,27 @@ package org.triple_brain.module.common_utils;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 /*
 * Copyright Mozilla Public License 1.1
 */
 public class Uris {
-    public static String encodeURL(String URL) throws UnsupportedEncodingException {
-        return java.net.URLEncoder.encode(URL, "ISO-8859-1");
+    public static String encodeURL(String URL){
+        try{
+            return URLEncoder.encode(URL, "ISO-8859-1");
+        }catch(UnsupportedEncodingException e){
+            throw new RuntimeException(e);
+        }
     }
     public static String decodeURL(String URL) throws UnsupportedEncodingException{
-        return java.net.URLDecoder.decode(URL, "ISO-8859-1");
+        try{
+            return URLDecoder.decode(URL, "ISO-8859-1");
+        }catch(UnsupportedEncodingException e){
+            throw new RuntimeException(e);
+        }
+
     }
     public static URI get(String uri){
         try{
