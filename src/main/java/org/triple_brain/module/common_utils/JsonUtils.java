4086fd83254e9c8f896f5e46dec80cb8b7c4267d
Vincent Blouin <vincent.blouin@gmail.com>
2013-06-15 17:29:10 -0400

    prepared backend for inner map identifications and suggestions

--- src/main/java/org/triple_brain/module/common_utils/JsonUtils.java
+++ src/main/java/org/triple_brain/module/common_utils/JsonUtils.java
@@ -1,5 +1,6 @@
 package org.triple_brain.module.common_utils;
 
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
@@ -12,14 +13,23 @@ public class JsonUtils {
 
     public static JSONObject merge(JSONObject object1, JSONObject object2) throws JSONException {
         JSONObject merged = new JSONObject();
-        JSONObject[] objs = new JSONObject[] { object1, object2};
+        JSONObject[] objs = new JSONObject[]{object1, object2};
         for (JSONObject obj : objs) {
             Iterator it = obj.keys();
             while (it.hasNext()) {
-                String key = (String)it.next();
+                String key = (String) it.next();
                 merged.put(key, obj.get(key));
             }
         }
         return merged;
+    }
+
+    public static Boolean containsString(JSONArray array, String string) {
+        for (int i = 0; i < array.length(); i++) {
+            if (array.optString(i).equals(string)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
