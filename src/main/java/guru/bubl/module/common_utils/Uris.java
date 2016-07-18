82eb54dbfec643515aa9e3c654eb95ec656706bb
Vincent Blouin <vincent.blouin@gmail.com>
2016-07-18 11:34:28 -0400

    french accents allowed

--- src/main/java/guru/bubl/module/common_utils/Uris.java
+++ src/main/java/guru/bubl/module/common_utils/Uris.java
@@ -8,37 +8,49 @@ import java.io.UnsupportedEncodingException;
 import java.net.*;
 
 public class Uris {
-    public static String encodeURL(String URL){
-        try{
-            return URLEncoder.encode(URL, "ISO-8859-1");
-        }catch(UnsupportedEncodingException e){
+    public static String encodeURL(String URL) {
+        try {
+            return URLEncoder.encode(URL, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
     }
-    public static String encodeURL(URI uri){
+
+    public static String encodeURL(URI uri) {
         return encodeURL(uri.toString());
     }
 
-    public static String encodeURL(URL url){
+    public static String encodeURL(URL url) {
         return encodeURL(url.toString());
     }
 
-    public static String decodeUrl(String url){
-        try{
+    public static String decodeUrl(String url) {
+        try {
             return decodeUrlSafe(
                     url
             );
-        }catch(UnsupportedEncodingException e){
+        } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
     }
-    public static String decodeUrlSafe(String url) throws UnsupportedEncodingException{
-        return URLDecoder.decode(url, "ISO-8859-1");
+
+    public static URI decodeUriSafe(URI uri) throws UnsupportedEncodingException {
+        return URI.create(
+                URLDecoder.decode(
+                        uri.toString(),
+                        "UTF-8"
+                )
+        );
     }
-    public static URI get(String uri){
-        try{
+
+    public static String decodeUrlSafe(String url) throws UnsupportedEncodingException {
+        return URLDecoder.decode(url, "UTF-8");
+    }
+
+    public static URI get(String uri) {
+        try {
             return new URI(uri);
-        }catch (URISyntaxException e){
+        } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
     }
